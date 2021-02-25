4e44f9380aa665878dafd6c8b5937cca4d5bd37d
scafati98 <eugenio@muttdata.ai>
2021-02-25 11:11:57 -0300

    final version

--- src/hypermodern_python_setup/console.py
+++ src/hypermodern_python_setup/console.py
@@ -1,35 +1,24 @@
-# src/hypermodern_python/console.py
+"""Command-line interface."""
 import textwrap
 
 import click
-import requests
 
-from . import __version__
-
-
-API_URL = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language: str) -> None:
     """The hypermodern Python project."""
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        data = response.json()
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
-
-
-
-
-
-
-
-
-
-
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
